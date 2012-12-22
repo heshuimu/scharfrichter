5190042a2022bc710eb8749ee3e785c6908beb55
saxxonpike@gmail.com <saxxonpike@gmail.com@1c64fb9a-5ca7-a3eb-beb2-acf4dfbdce57>
2012-12-22 02:03:16 +0000

    minor update

---
+++ BemaniToBMSGold/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConvertHelper;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BemaniToBMSGold
+{
+	class Program
+	{
+		static int quantizeMeasure = 16;
+		static long unitNumerator = 100;
+		static long unitDenominator = 6004;
+
+		static void Main(string[] args)
+		{
+			ConvertFunctions.BemaniToBMS(args, unitNumerator, unitDenominator, quantizeMeasure);
+		}
+	}
+}
---
+++ Scharfrichter/Archives/BemaniIFS.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Scharfrichter.Codec.Archives
+{
+	public class BemaniIFS : Archive
+	{
+		private List<byte[]> files = new List<byte[]>();
+
+		private struct Entry
+		{
+			public Int32 Offset;
+			public Int32 Length;
+			public Int32 Meta;
+		}
+
+		public override byte[][] RawData
+		{
+			get
+			{
+				return files.ToArray();
+			}
+			set
+			{
+				files.Clear();
+				files.AddRange(value);
+			}
+		}
+
+		public override int RawDataCount
+		{
+			get
+			{
+				return files.Count;
+			}
+		}
+
+		static public BemaniIFS Read(Stream source)
+		{
+			List<Entry> entries = new List<Entry>();
+			BinaryReader reader = new BinaryReader(source);
+			BemaniIFS result = new BemaniIFS();
+
+			// header length is 0x28 bytes
+			reader.ReadInt32();
+			reader.ReadInt32();
+			reader.ReadInt32();
+			reader.ReadInt32();
+			Int32 headerLength = SwapEndian(reader.ReadInt32());
+			reader.ReadInt32();
+			reader.ReadInt32();
+			reader.ReadInt32();
+			reader.ReadInt32();
+			reader.ReadInt32();
+
+			Console.WriteLine("Header length: " + headerLength.ToString());
+
+			// read table A
+			Int32 tableALength = SwapEndian(reader.ReadInt32());
+			Console.WriteLine("Table A length: " + tableALength.ToString());
+			using (MemoryStream mem = new MemoryStream(reader.ReadBytes(tableALength)))
+			{
+				BinaryReader tableReader = new BinaryReader(mem);
+			}
+
+			// read table B
+			Int32 totalDataLength = 0;
+			Int32 tableBLength = SwapEndian(reader.ReadInt32());
+			Console.WriteLine("Table B length: " + tableBLength.ToString());
+			using (MemoryStream mem = new MemoryStream(reader.ReadBytes(tableBLength)))
+			{
+				BinaryReader tableReader = new BinaryReader(mem);
+				Int32 metaDataLength = SwapEndian(tableReader.ReadInt32());
+				byte[] metaData = tableReader.ReadBytes(metaDataLength);
+				totalDataLength = SwapEndian(tableReader.ReadInt32());
+				tableReader.ReadInt32();
+				Int32 tableCount = (tableBLength - (4 + metaDataLength + 8)) / 0xC;
+				Console.WriteLine("Table B entries: " + tableCount.ToString());
+				for (int i = 0; i < tableCount; i++)
+				{
+					Entry entry = new Entry();
+					entry.Offset = SwapEndian(tableReader.ReadInt32());
+					entry.Length = SwapEndian(tableReader.ReadInt32());
+					entry.Meta = SwapEndian(tableReader.ReadInt32());
+					entries.Add(entry);
+				}
+			}
+
+			int headerPadding = headerLength - (0x28 + 4 + tableALength + 4 + tableBLength);
+			if (headerPadding > 0)
+				reader.ReadBytes(headerPadding);
+
+			// read data chunk
+			using (MemoryStream mem = new MemoryStream(reader.ReadBytes(totalDataLength)))
+			{
+				for (int i = 0; i < entries.Count; i++)
+				{
+					byte[] data;
+					Entry entry = entries[i];
+
+					mem.Position = entry.Offset;
+					data = new byte[entry.Length];
+					mem.Read(data, 0, data.Length);
+					result.files.Add(data);
+				}
+			}
+
+			return result;
+		}
+
+		static private Int32 SwapEndian(Int32 operand)
+		{
+			Int32 a = (operand >> 0) & (0xFF);
+			Int32 b = (operand >> 8) & (0xFF);
+			Int32 c = (operand >> 16) & (0xFF);
+			Int32 d = (operand >> 24) & (0xFF);
+			Int32 result = a;
+			result <<= 8;
+			result |= b;
+			result <<= 8;
+			result |= c;
+			result <<= 8;
+			result |= d;
+			return result;
+		}
+	}
+}
