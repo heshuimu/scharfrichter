2ec08020aa742dac2c47692d6a0586812272524f
saxxonpike@gmail.com <saxxonpike@gmail.com@1c64fb9a-5ca7-a3eb-beb2-acf4dfbdce57>
2012-12-29 01:16:17 +0000

    add K054539 decoder and DJMain archive

---
+++ trunk/Scharfrichter/Archives/DJMain.cs
@@ -0,0 +1,25 @@
+﻿using Scharfrichter.Codec.Charts;
+using Scharfrichter.Codec.Sounds;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Scharfrichter.Codec.Archives
+{
+	public class DJMainChunk : Archive
+	{
+		private List<Chart> charts;
+		private List<Sound> sounds;
+
+		static public Archive Read(Stream source, int[] chartOffsets, int[] sampleInfoOffsets, int sampleDataOffset)
+		{
+			byte[] rawData = new byte[0x1000000];
+			source.Read(rawData, 0, rawData.Length);
+
+
+		}
+	}
+}
