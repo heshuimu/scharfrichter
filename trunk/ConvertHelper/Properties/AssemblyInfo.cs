bc17806ec3be120a051854946bb5d4649d4b9a15
saxxonpike@gmail.com <saxxonpike@gmail.com@1c64fb9a-5ca7-a3eb-beb2-acf4dfbdce57>
2013-01-05 01:13:22 +0000

    remove copyright info

--- trunk/ConvertHelper/Properties/AssemblyInfo.cs
+++ trunk/ConvertHelper/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConvertHelper")]
+[assembly: AssemblyTitle("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConvertHelper")]
-[assembly: AssemblyCopyright("Pegasystem")]
+[assembly: AssemblyCompany("DJSLACKERS")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
