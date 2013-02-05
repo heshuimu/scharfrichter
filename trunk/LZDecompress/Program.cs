b29a96dbb9c57e52953b81056546a4fc6b67935c
saxxonpike@gmail.com <saxxonpike@gmail.com@1c64fb9a-5ca7-a3eb-beb2-acf4dfbdce57>
2013-02-05 18:47:21 +0000

    Firebeat decompression.

--- trunk/LZDecompress/Program.cs
+++ trunk/LZDecompress/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Scharfrichter.Codec;
 using Scharfrichter.Codec.Compression;
+using Scharfrichter.Common;
 
 using System;
 using System.Collections.Generic;
@@ -13,20 +14,42 @@ namespace LZDecompress
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("DJSLACKERS - LZDecompress");
+            Splash.Show("LZDecompress");
 			args = Subfolder.Parse(args);
 
-			if (args.Length > 0)
+            if (args.Length == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Usage: LZDecompress <input file>");
+                Console.WriteLine();
+                Console.WriteLine("Drag and drop with files and folders is fully supported for this application.");
+                Console.WriteLine("The output file will be the input file appended with a .DEC extension.");
+                return;
+            }
+
+            if (args.Length > 0)
 			{
-				byte[] data = File.ReadAllBytes(args[0]);
-				using (MemoryStream mem = new MemoryStream(data))
-				{
-					using (MemoryStream output = new MemoryStream())
-					{
-						BemaniLZ.Decompress(mem, output);
-						File.WriteAllBytes(Path.GetFileNameWithoutExtension(args[0]) + ".out", output.ToArray()); 
-					}
-				}
+                foreach (string filename in args)
+                {
+                    try
+                    {
+                        Console.Write("Decompressing " + filename + "... ");
+                        byte[] data = File.ReadAllBytes(filename);
+                        using (MemoryStream mem = new MemoryStream(data))
+                        {
+                            using (MemoryStream output = new MemoryStream())
+                            {
+                                BemaniLZ.Decompress(mem, output);
+                                File.WriteAllBytes(filename + ".dec", output.ToArray());
+                            }
+                        }
+                        Console.WriteLine("Success.");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("FAILED.");
+                    }
+                }
 			}
 		}
 	}
--- trunk/Scharfrichter/Compression/BemaniLZSS2.cs
+++ trunk/Scharfrichter/Compression/BemaniLZSS2.cs
@@ -8,23 +8,40 @@ using System.Text;
 
 namespace Scharfrichter.Codec.Compression
 {
+    public enum BemaniLZSS2Type
+    {
+        GCZ,
+        Firebeat
+    }
+
+    public struct BemaniLZSS2Properties
+    {
+        public int ringBufferOffset;
+        public int ringBufferSize;
+        public BemaniLZSS2Type type;
+    }
+
 	static public class BemaniLZSS2
 	{
-		static public void Compress(Stream source, Stream target, int length, out int decompLength)
+		static public void Compress(Stream source, Stream target, int length, BemaniLZSS2Properties props)
 		{
-			decompLength = 0;
 		}
 
-		static public void Decompress(Stream source, Stream target, int length, int decompLength)
+		static public void Decompress(Stream source, Stream target, int length, int decompLength, BemaniLZSS2Properties props)
 		{
-			byte[] ring = new byte[0x1000];
-			int ring_pos = 0x0FEE; 
+			byte[] ring = new byte[props.ringBufferSize];
+			int ring_pos = props.ringBufferOffset; 
 			int chunk_offset; 
 			int chunk_length; 
 			int control_word = 1;
+            int controlBitsLeft = 0;
+            int controlBitMask = 0x1;
 			byte cmd1; 
 			byte cmd2;
 			byte data;
+
+            if (decompLength <= 0)
+                decompLength = int.MaxValue;
 
 			BinaryReaderEx sourceReader = new BinaryReaderEx(source);
 			BinaryWriterEx writer = new BinaryWriterEx(target);
@@ -33,46 +50,92 @@ namespace Scharfrichter.Codec.Compression
 			{
 				BinaryReaderEx reader = new BinaryReaderEx(mem);
 
-				while (decompLength > 0) {
-					if (control_word == 1) {
+				while (decompLength > 0 && length > 0) 
+                {
+                    if (controlBitsLeft == 0)
+                    {
 						/* Read a control byte */
-						control_word = 0x100 | reader.ReadByte();
+						control_word = reader.ReadByte();
+                        length--;
+                        controlBitsLeft = 8;
 					}
 
 					/* Decode a byte according to the current control byte bit */
-					if ((control_word & 1) != 0) {
+                    if ((control_word & controlBitMask) != 0)
+                    {
 						/* Straight copy, store into history ring */
 						data = reader.ReadByte();
+                        length--;
 
 						writer.Write(data);
 						ring[ring_pos] = data;
 
-						ring_pos = (ring_pos + 1) % 0x1000;
+                        ring_pos = (ring_pos + 1) % props.ringBufferSize;
 						decompLength--;
-					} else {
+					} 
+                    else 
+                    {
 						/* Reference to data in ring buffer */
-						cmd1 = reader.ReadByte();
-						cmd2 = reader.ReadByte();
 
-						chunk_length = (cmd2 & 0x0F) + 3;
-						chunk_offset = ((cmd2 & 0xF0) << 4) | cmd1;
+                        switch (props.type)
+                        {
+                            case BemaniLZSS2Type.Firebeat:
+						        cmd1 = reader.ReadByte();
+						        cmd2 = reader.ReadByte();
+                                length -= 2;
+                                chunk_length = (cmd1 & 0x0F) + 3;
+                                chunk_offset = (((int)cmd1 & 0xF0) << 4) + (int)cmd2;
+                                chunk_offset = ring_pos - chunk_offset;
+                                while (chunk_offset < 0)
+                                    chunk_offset += props.ringBufferSize;
+                                break;
+                            case BemaniLZSS2Type.GCZ:
+						        cmd1 = reader.ReadByte();
+						        cmd2 = reader.ReadByte();
+                                length -= 2;
+						        chunk_length = (cmd2 & 0x0F) + 3;
+						        chunk_offset = (((int)cmd2 & 0xF0) << 4) | cmd1;
+                                break;
+                            default:
+                                return;
+                        }
 
-						for ( ; chunk_length > 0 && length > 0 ; chunk_length--) {
+						for ( ; chunk_length > 0 && length > 0 ; chunk_length--) 
+                        {
 							/* Copy historical data to output AND current ring pos */
 							writer.Write(ring[chunk_offset]);
 							ring[ring_pos] = ring[chunk_offset];
 
 							/* Update counters */
-							chunk_offset = (chunk_offset + 1) % 0x1000;
-							ring_pos = (ring_pos + 1) % 0x1000;
+                            chunk_offset = (chunk_offset + 1) % props.ringBufferSize;
+                            ring_pos = (ring_pos + 1) % props.ringBufferSize;
 							decompLength--;
 						}
 					}
 
-					/* Get next control bit */
-					control_word >>= 1;
+                    /* Get next control bit */
+                    control_word >>= 1;
+                    controlBitsLeft--;
 				}
 			}
 		}
+
+        static public void DecompressFirebeat(Stream source, Stream target, int length, int decompLength)
+        {
+            BemaniLZSS2Properties props = new BemaniLZSS2Properties();
+            props.ringBufferOffset = 0xFFE;
+            props.ringBufferSize = 0x1000;
+            props.type = BemaniLZSS2Type.Firebeat;
+            Decompress(source, target, length, decompLength, props);
+        }
+
+        static public void DecompressGCZ(Stream source, Stream target, int length, int decompLength)
+        {
+            BemaniLZSS2Properties props = new BemaniLZSS2Properties();
+            props.ringBufferOffset = 0xFFE;
+            props.ringBufferSize = 0x1000;
+            props.type = BemaniLZSS2Type.GCZ;
+            Decompress(source, target, length, decompLength, props);
+        }
 	}
 }
